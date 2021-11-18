d2bc4607b6fb0436985f951678bb8fea1e87e979
heejeong <jennachoi27@gmail.com>
2021-11-18 17:22:11 +0900

    배열에 n개만큼 넣기

---
+++ src/level1/ArrPrac.java
@@ -0,0 +1,13 @@
+package level1;
+
+public class ArrPrac {
+    public long[] solution(int x, int n) {
+        long[] answer = {};
+
+        /*for(int i=x; i<n+1; i++) {
+            answer.
+        }*/
+
+        return answer;
+    }
+}
