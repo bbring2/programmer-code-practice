b9f295014281a678436e63ed1416f6be42d54b53
heejeong <jennachoi27@gmail.com>
2021-11-18 12:48:48 +0900

    전화번호 뒷 4자리 빼고 가리기

---
+++ src/level1/CoverMobileNum.java
@@ -0,0 +1,12 @@
+package level1;
+
+public class CoverMobileNum {
+    public String solution(String mobileNum) {
+        String answer = "";
+        for(int i = 0; i < mobileNum.length();i++) {
+            if (i<mobileNum.length()-4)
+                answer+="*";
+            else answer+=mobileNum.charAt(i);
+        } return answer;
+    }
+}
