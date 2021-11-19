56e4d2144a18568163ca97c7e3717a3bd826c13c
heejeong <jennachoi27@gmail.com>
2021-11-19 13:51:23 +0900

    수박수박수박수

---
+++ src/level1/Watermelon.java
@@ -0,0 +1,19 @@
+package level1;
+
+public class Watermelon {
+    public static String watermelon(int n) {
+        String answer = "";
+        for(int i=0; i<n; i++) {
+            if(i%2 == 0) {
+                answer += "수";
+            } else {
+                answer += "박";
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(watermelon(7));
+    }
+}
