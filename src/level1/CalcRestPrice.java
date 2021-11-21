e3f33bfb214d96bc0274e2813f487f1fc003d387
heejeong-choi <jennachoi27@gmail.com>
2021-11-21 23:23:51 +0900

    모자란 금액 계산하기

---
+++ src/level1/CalcRestPrice.java
@@ -0,0 +1,23 @@
+package level1;
+
+/*
+* 원래 이용료 : price원
+* 놀이기구를 N번째 이용하면 N * price로 받음(첫번째가 100원이었다면 두번째는 200원 이런식)
+* 놀이기구를 count번 타게되면 현재 자신이 갖고 있는 금액에서 얼마나 모자란지 return하기 > 금액 안부족한경우 0
+* 놀이기구 이용료 price : 1=<price=<2500
+* 처음 갖고 있던 금액 money : 1=<money=<1000000000
+* 놀이기구 이용횟수 count : 1=<count=<2500
+*
+* ex) price : 3, money : 20, count : 4, result : 10
+*
+* */
+public class CalcRestPrice {
+    public static long solution(int price, int money, int count) {
+        long answer = -1;
+
+
+
+        return answer;
+    }
+
+}
