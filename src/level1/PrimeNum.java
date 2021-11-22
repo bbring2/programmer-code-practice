fe4b423943253fb1c7d734560c88b71f91899273
heejeong <jennachoi27@gmail.com>
2021-11-22 09:59:37 +0900

    소수만들기

---
+++ src/level1/PrimeNum.java
@@ -0,0 +1,9 @@
+package level1;
+
+public class PrimeNum {
+    public static int solution(int[] nums) {
+        int answer = -1;
+
+        return answer;
+    }
+}
---
+++ src/level2/ExpressMath.java
@@ -0,0 +1,9 @@
+package level2;
+
+public class ExpressMath {
+    public int Solution(int n) {
+        int answer = 0;
+
+        return answer;
+    }
+}
