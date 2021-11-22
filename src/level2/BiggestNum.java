f966c40c61d99f18782b10aeff5ea478fa2e05c3
heejeong <jennachoi27@gmail.com>
2021-11-22 13:12:56 +0900

    가장 큰 수 만들기

---
+++ src/level2/BiggestNum.java
@@ -0,0 +1,27 @@
+package level2;
+
+/*
+* 정수를 이어 붙여 만들 수 있는 가장 큰 수
+* */
+
+import java.util.Arrays;
+import java.util.Collections;
+
+public class BiggestNum {
+    public static String solution(int[] numbers) {
+        String tmp = Arrays.toString(numbers).replace(", ","").replace("[","").replace("]","");
+        String[] tmpArr = tmp.split("");
+
+        Arrays.sort(tmpArr);
+        Collections.reverse(Arrays.asList(tmpArr));
+
+        String answer = Arrays.toString(tmpArr).replace(", ", "").replace("[", "").replace("]", "");
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,10,99};
+        System.out.println(solution(arr));
+    }
+}
