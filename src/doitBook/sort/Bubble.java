dd14c750c18afd50331910c2945fba77861a18e7
heejeong-choi <jennachoi27@gmail.com>
2022-08-18 23:19:26 +0900

    버블 정렬

---
+++ src/doitBook/sort/Bubble.java
@@ -0,0 +1,45 @@
+package doitBook.sort;
+
+import java.util.Scanner;
+
+public class Bubble {
+
+    static void swap(int[] a, int idx1, int idx2) {
+        int numInArr = a[idx1];
+        a[idx1] = a[idx2];
+        a[idx2] = numInArr;
+    }
+
+    static void bubbleSort(int[] a, int n) {
+        for(int i=0; i<n-1; i++) {
+            for(int j=n-1; j>i; j--) {
+                if(a[j-1] > a[j]) {
+                    swap(a, j-1, j);
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Bubble sort ver1");
+        System.out.println("factors: ");
+
+        int nx = scanner.nextInt();
+        int[] x = new int[nx];
+
+        for(int i=0; i<nx; i++) {
+            System.out.println("x[" + i + "]");
+            x[i] = scanner.nextInt();
+        }
+
+        bubbleSort(x, nx);
+
+        System.out.println("오름차순으로 정렬 하였습니다.");
+
+        for(int i=0; i<nx; i++) {
+            System.out.println("x[" + i + "]" + x[i]);
+        }
+    }
+}
---
+++ src/hackkerank/easy/string/CurrencyFormat.java
@@ -0,0 +1,11 @@
+package hackkerank.easy.string;
+
+import java.text.NumberFormat;
+
+public class CurrencyFormat {
+    public static void main(String[] args) {
+        Double d = 1234.1234;
+        long numberFormat = d.longValue();
+        System.out.println(numberFormat);
+    }
+}
